f3cc2ac584546103a3e8199f8dbe80e67b18d609
garox <gabriel@garox.org>
2017-06-04 16:42:46 -0400

    REPL

---
+++ cREPL/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+  printf("Hello world!\n");
+  printf("Numeros!\n");
+  int a = GetInt();
+  printf("a es %i\n", a);
+  int b = GetInt();
+  printf("b es %i\n", b);
+  int c = GetInt();
+  printf("c es %i\n", c);
+  return 0;
+}
