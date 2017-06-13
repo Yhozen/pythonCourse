d40dc610df0a37ab4f8951fab8270950d4c9eea8
garox <gabriel@garox.org>
2017-06-12 23:05:03 -0400

    antes de cagarla

--- cREPL/main.js
+++ cREPL/main.js
@@ -2,29 +2,6 @@ const express = require('express')
 const app = express()
 const { compileNRun } = require('./compile')
 
-const text = `#include <stdio.h>
-#include <cs50.h>
-
-int fibonacci(int n) {
-  if (n==0 || n ==1) {
-    return n;
-  } else {
-    return fibonacci(n-1)+fibonacci(n-2);
-  }
-}
-
-int main() {
-  printf("Hello world!\\n");
-  printf("Numeros!\\n");
-  int a = 2;
-  printf("a es %i\\n", a);
-  int b = 3;
-  printf("b es %i\\n", b);
-  int c = 30;
-  printf("c es %i\\n", fibonacci(c));
-  return 0;
-}`
-
 const port = (process.env.PORT || 8000)
 
 app.set('json spaces', 2)
@@ -43,3 +20,26 @@ app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
     res.json(compileNRun('test',text))
 })
+
+// #include <stdio.h>
+// #include <cs50.h>
+//
+// int fibonacci(int n) {
+//   if (n==0 || n ==1) {
+//     return n;
+//   } else {
+//     return fibonacci(n-1)+fibonacci(n-2);
+//   }
+// }
+//
+// int main() {
+//   printf("Hello world!\n");
+//   printf("Numeros!\n");
+//   int a = 2;
+//   printf("a es %i\n", a);
+//   int b = 3;
+//   printf("b es %i\n", b);
+//   int c = 30;
+//   printf("c es %i\n", fibonacci(c));
+//   return 0;
+// }
--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    let { value } = event.target
+    this.setState({value})
+  }
+
   render() {
     return (
-      <div className="container">
-      <div className="card">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="images/office.jpg"/>
+      <div>
+        <nav>
+         <div className="nav-wrapper">
+           <a href="#" className="brand-logo">Logo</a>
+           <ul id="nav-mobile" className="right hide-on-med-and-down">
+             <li><a href="sass.html">Sass</a></li>
+             <li><a href="badges.html">Components</a></li>
+             <li><a href="collapsible.html">JavaScript</a></li>
+           </ul>
+         </div>
+       </nav>
+         <div className="row">
+            <form className="col s6">
+              <div className="row">
+                <div className="input-field col s12">
+                  <i className="material-icons prefix">mode_edit</i>
+                  <textarea value={this.state.value} onChange={this.handleChange} id="icon_prefix2" className="materialize-textarea"></textarea>
+                  <label for="icon_prefix2">First Name</label>
+                </div>
+              </div>
+            </form>
+            <div className="col s6">
+              {this.state.value}
+
+            </div>
           </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-            <p><a href="#">This is a link</a></p>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          </div>
-        </div>
       </div>
     );
   }
