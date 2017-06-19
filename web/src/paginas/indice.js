48c7e25585a58ab23a51f0189cc5b0068cf3372b
garox <gabriel@garox.org>
2017-06-18 21:48:40 -0400

    router en child

--- web/src/paginas/indice.js
+++ web/src/paginas/indice.js
@@ -14,119 +14,10 @@ class Indice extends Component {
               <p>{Clase0.brief}</p>
             </div>
             <div className="card-action">
-              <a>Ir a la clase 0!</a>
+              <a onClick={() => this.props.router(<Clases clase={Clase0}/>)}>Ir a la clase 0!</a>
             </div>
           </div>
         </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase1.title}</span>
-              <p>{Clase1.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 1!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase2.title}</span>
-              <p>{Clase2.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 2!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase3.title}</span>
-              <p>{Clase3.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 3!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase4.title}</span>
-              <p>{Clase4.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 4!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase5.title}</span>
-              <p>{Clase5.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 5!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase6.title}</span>
-              <p>{Clase6.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 6!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase7.title}</span>
-              <p>{Clase7.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 7!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase8.title}</span>
-              <p>{Clase8.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 8!</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col s4">
-        <div className="card red lighten-1">
-            <div className="card-content white-text">
-              <span className="card-title">{Clase9.title}</span>
-              <p>{Clase9.brief}</p>
-            </div>
-            <div className="card-action">
-              <a>Ir a la clase 9!</a>
-            </div>
-          </div>
-        </div>
-
       </div>
 
 
@@ -136,3 +27,108 @@ class Indice extends Component {
 }
 
 export default Indice;
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase1.title}</span>
+//       <p>{Clase1.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 1!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase2.title}</span>
+//       <p>{Clase2.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 2!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase3.title}</span>
+//       <p>{Clase3.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 3!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase4.title}</span>
+//       <p>{Clase4.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 4!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase5.title}</span>
+//       <p>{Clase5.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 5!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase6.title}</span>
+//       <p>{Clase6.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 6!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase7.title}</span>
+//       <p>{Clase7.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 7!</a>
+//     </div>
+//   </div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase8.title}</span>
+//       <p>{Clase8.brief}</p>
+//     </div>
+//     <div>
+// </div>
+//
+// <div className="col s4">
+// <div className="card red lighten-1">
+//     <div className="card-content white-text">
+//       <span className="card-title">{Clase9.title}</span>
+//       <p>{Clase9.brief}</p>
+//     </div>
+//     <div className="card-action">
+//       <a>Ir a la clase 9!</a>
+//     </div>
+//   </div>
+// </div>
