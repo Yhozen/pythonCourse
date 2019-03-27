6a0441f19b508df98b3a632f129e634c67ca2968
Gabriel <gabriel@garox.org>
2019-03-27 19:19:24 -0300

    use persist (snippet from f)

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useReducer, useContext } from 'react'
+import { usePersistedContext, usePersistedReducer } from './usePersist'
 
 const initialState = {}
-
 export const Store = React.createContext(initialState)
 
-function reducer (state, action) {
+const reducer = (state, action) => {
   switch (action.type) {
     case 'USER_LOGIN':
       return { ...state, user: action.payload }
@@ -14,7 +14,8 @@ function reducer (state, action) {
 }
 
 export function StoreProvider (props) {
-  const [ state, dispatch ] = React.useReducer(reducer, initialState)
+  const globalStore = usePersistedContext(useContext(Store))
+  const [ state, dispatch ] = usePersistedReducer(useReducer(reducer, globalStore))
   const value = { state, dispatch }
   return <Store.Provider value={value}>{props.children}</Store.Provider>
 }
