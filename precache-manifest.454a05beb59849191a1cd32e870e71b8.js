dd3ebc9878cddc5cf159dae31f8faa7f2f9820ad
Gabriel <gabriel@garox.org>
2019-04-01 17:21:17 -0300

    Updates

---
+++ precache-manifest.454a05beb59849191a1cd32e870e71b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5877b29e9bb320f69c14",
+    "url": "/static/js/main.a09f5c85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0367457728c69a8c41d",
+    "url": "/static/js/2.00f4f22b.chunk.js"
+  },
+  {
+    "revision": "3abd1ba95c7dd83e2ac9053e6924c8f0",
+    "url": "/index.html"
+  }
+];
